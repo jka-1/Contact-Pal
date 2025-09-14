b5ae4123b85c9cc12ef528756ba0fc45bba851b1
arthadkulkarni <arthadkulk@gmail.com>
2025-09-13 20:07:08 -0400

    AddContact and SearchContact added

---
+++ AddContact.php
@@ -0,0 +1,51 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+	header('Access-Control-Allow-Headers: Content-Type, Authorization');
+	header('Content-Type: application/json');
+
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    	http_response_code(200);
+    	exit();
+	}
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "Team2", "CONTACTPALDB");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserId,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $phone, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
